86b0100832f32184263eec45f8174a4b88fdc6c5
Israel Moreno <se.israel.mc@gmail.com>
2020-05-06 00:25:52 -0500

    constantes
    Constantes para textos agregadas

---
+++ lib/utils/textApp.dart
@@ -0,0 +1,4 @@
+const text = {
+  'loginTitle' : 'Descubre y comparte cosas interesantes',
+  'loginIntro' : 'La comunidad Flutter GDL comparte cosas interesantes y entretenidas. Se parte ahora. Registrate!'
+};
